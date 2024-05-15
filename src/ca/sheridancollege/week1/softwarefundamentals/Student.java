9e71c987ee3e4e3ded617b099ac0b6afcc0d4dd9
gurgursi <gurgursi@sheridancollege.ca>
2024-05-15 15:56:31 -0400

    Just Showing off

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  *@Modified by Gursimran
+ *LOL
+ *OHHHH New Change
  */
 package ca.sheridancollege.week1.softwarefundamentals;
 
